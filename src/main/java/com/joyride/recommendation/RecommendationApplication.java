a331641d45a4c7c2a9dce52caeff3bff7954aa05
sihyunjojo <si4018@naver.com>
2024-10-07 03:44:57 +0900

    feat: 유레카 서버 설정 추가

--- src/main/java/com/joyride/recommendation/RecommendationApplication.java
+++ src/main/java/com/joyride/recommendation/RecommendationApplication.java
@@ -2,6 +2,7 @@ package com.joyride.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
